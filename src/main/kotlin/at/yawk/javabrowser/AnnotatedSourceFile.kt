a1d95cfda168240807ec21b4b75e162e751d90e8
yawkat <me@yawk.at>
2018-05-30 07:51:28 +0200

    Line numbers

--- src/main/kotlin/at/yawk/javabrowser/AnnotatedSourceFile.kt
+++ src/main/kotlin/at/yawk/javabrowser/AnnotatedSourceFile.kt
@@ -1,8 +1,10 @@
 package at.yawk.javabrowser
 
 import org.eclipse.jdt.core.dom.ASTNode
+import org.jsoup.nodes.Element
 import org.jsoup.nodes.Node
 import org.jsoup.nodes.TextNode
+import org.jsoup.parser.Tag
 
 /**
  * @author yawkat
@@ -21,21 +23,46 @@ class AnnotatedSourceFile(val text: String) {
     }
 
     private fun sort() {
-        entries.sortBy { it.start }
+        entries.sortWith(Comparator.comparingInt { it: Entry -> it.start }.thenComparingInt { it -> it.length.inv() })
     }
 
     fun toHtml(toNode: (SourceAnnotation, List<Node>) -> List<Node>): List<Node> {
         sort()
+        var line = 1
         var entryIndex = 0
         var textIndex = 0
+
+        fun lineMarker(out: MutableList<Node>) {
+            val link = Element(Tag.valueOf("a"), AnnotatedSourceFile.URI)
+            val lineStr = line.toString()
+            link.attr("href", "#$lineStr")
+            link.attr("id", lineStr)
+            link.attr("class", "line")
+            link.attr("data-line", lineStr)
+            out.add(link)
+
+            line++
+        }
+
+        fun consumeText(out: MutableList<Node>, until: Int) {
+            while (textIndex < until) {
+                val nextLine = text.indexOf('\n', textIndex)
+                if (nextLine != -1 && nextLine < until) {
+                    out.add(TextNode(text.substring(textIndex, nextLine + 1), URI))
+                    textIndex = nextLine + 1
+                    lineMarker(out)
+                } else {
+                    out.add(TextNode(text.substring(textIndex, until), URI))
+                    textIndex = until
+                }
+            }
+        }
 
         fun consumeUntil(out: MutableList<Node>, until: Int) {
             while (textIndex < until) {
                 if (entries.size > entryIndex && entries[entryIndex].start < until) {
                     val nextEntry = entries[entryIndex++]
-                    if (nextEntry.start != textIndex) {
-                        out.add(TextNode(text.substring(textIndex, nextEntry.start), URI))
-                    }
+                    consumeText(out, nextEntry.start)
                     textIndex = nextEntry.start
 
                     if (nextEntry.start + nextEntry.length > until) throw AssertionError(entries)
@@ -44,13 +71,14 @@ class AnnotatedSourceFile(val text: String) {
                     consumeUntil(nest, nextEntry.start + nextEntry.length)
                     out.addAll(toNode(nextEntry.annotation, nest))
                 } else {
-                    out.add(TextNode(text.substring(textIndex, until), URI))
+                    consumeText(out, until)
                     textIndex = until
                 }
             }
         }
 
         val out = ArrayList<Node>()
+        lineMarker(out)
         consumeUntil(out, text.length)
         return out
     }
