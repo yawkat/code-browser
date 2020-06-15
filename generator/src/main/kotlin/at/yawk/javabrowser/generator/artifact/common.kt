e799a05903ee7e46bbe89dbea26465aaaf84d3ae
Jonas Konrad <me@yawk.at>
2020-06-15 10:21:46 +0200

    Index fixes

--- generator/src/main/kotlin/at/yawk/javabrowser/generator/artifact/common.kt
+++ generator/src/main/kotlin/at/yawk/javabrowser/generator/artifact/common.kt
@@ -8,7 +8,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
-const val COMPILER_VERSION = 34
+const val COMPILER_VERSION = 35
 
 fun getArtifactId(artifact: ArtifactConfig) = when (artifact) {
     is ArtifactConfig.Java -> "java/${artifact.version}"
--- generator/src/main/resources/at/yawk/javabrowser/generator/DataIndex.sql
+++ generator/src/main/resources/at/yawk/javabrowser/generator/DataIndex.sql
@@ -1,29 +1,24 @@
 alter table sourceFiles add primary key (realm, artifactId, path);
-alter table _sourceFiles0 add foreign key (artifactId) references artifacts;
-alter table _sourceFiles1 add foreign key (artifactId) references artifacts;
+alter table sourceFiles add foreign key (artifactId) references artifacts;
 alter table sourceFileLexemes add foreign key (realm, artifactId, sourceFile) references sourceFiles;
 alter table sourceFileLexemesNoSymbols add foreign key (realm, artifactId, sourceFile) references sourceFiles;
 create index on sourceFileLexemes using gin(lexemes);
 create index on sourceFileLexemesNoSymbols using gin(lexemes);
 
 alter table bindings add primary key (realm, binding, artifactId) include (sourceFile);
-alter table _bindings0 add foreign key (artifactId) references artifacts;
-alter table _bindings1 add foreign key (artifactId) references artifacts;
-alter table _bindings0 add foreign key (realm, artifactId, sourceFile) references sourceFiles;
-alter table _bindings1 add foreign key (realm, artifactId, sourceFile) references sourceFiles;
-alter table _bindings0 add foreign key (realm, artifactId, parent) references bindings;
-alter table _bindings1 add foreign key (realm, artifactId, parent) references bindings;
+alter table bindings add foreign key (artifactId) references artifacts;
+alter table bindings add foreign key (realm, artifactId, sourceFile) references sourceFiles;
+alter table bindings add foreign key (realm, parent, artifactId) references bindings;
 
 alter table binding_references add primary key (realm, sourceArtifactId, sourceFile, sourceFileId);
-alter table _binding_references0 add foreign key (realm, sourceArtifactId, sourceFile) references sourceFiles;
-alter table _binding_references1 add foreign key (realm, sourceArtifactId, sourceFile) references sourceFiles;
+alter table binding_references add foreign key (realm, sourceArtifactId, sourceFile) references sourceFiles;
 create index on _binding_references0 (targetBinding, type, sourceArtifactId);
 create index on _binding_references1 (targetBinding, type, sourceArtifactId);
 -- Due to a planner limitation ( https://commitfest.postgresql.org/15/1124/ ), postgres is currently unable to
 -- sort by (type, sourceArtifactId, sourceFile, sourceFileId) using the index over
 -- (targetBinding, type, sourceArtifactId). For this reason we add this new index.
-create index binding_references0_targetbinding_type_sourceartifactid_s_idx on _binding_references0 (targetBinding, type, sourceArtifactId, sourceFile, sourceFileId);
-create index binding_references1_targetbinding_type_sourceartifactid_s_idx on _binding_references1 (targetBinding, type, sourceArtifactId, sourceFile, sourceFileId);
+create index on _binding_references0 (targetBinding, type, sourceArtifactId, sourceFile, sourceFileId);
+create index on _binding_references1 (targetBinding, type, sourceArtifactId, sourceFile, sourceFileId);
 
 create unique index on binding_references_count_view (targetBinding, type, sourceArtifactId);
 create unique index on packages_view (artifactId, name);
